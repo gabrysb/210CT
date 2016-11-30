87a4fe8f99e4cece0663c4c46b11f9a7729bed19
Barbara <gabrysb@uni.coventry.ac.uk>
2016-11-30 16:55:41 +0000

    Update Question10.py

--- Question10.py
+++ Question10.py
@@ -1,25 +1,26 @@
+""" Given a sequence of n integer numbers, extract the sub-sequence of maximum length which is in ascending order."""
+
 L = [1,2,3,4,1,5,1,6,7]
 
-
 def subseq ():
-temp = [ ]
-  	count = 0
-  	big= 0
- 	for i in range(1, len(L)-1):
-   		if L[i] >= L[i-1]:
-     			count =+1
+ 	"""This function runs through the list and if it hits a value where the next value is lower, then it saves the previous values and the length of the sub-list, and clears those temp values and carries on iterating until it hits the end of the list."""
+temp = []
+ 	count = 0
+ 	big= 0
+	for i in range(1, len(L)-1):
+  		if L[i] >= L[i-1]:
+    			count =+1
 			temp.append(L[i])
-    		elif L[i] < L[i-1]:
-      			if count > big:
-       				big = count
-        				count = 0
+   		elif L[i] < L[i-1]:
+     			if count > big:
+      			big = count
+       			count = 0
 				arr = temp
-				temp = [ ]
-     		 	else:
-        				count = 0
-				temp = [ ]
-  	return (big, arr)
-
+				temp = []
+    		 	else:
+       			count = 0
+				temp = []
+ 	return (big, arr)
 
 x = subseq()
 print(x)

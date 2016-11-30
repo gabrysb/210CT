9467825a376ffd0a3a6bf99b0235c684edee61d5
Barbara <gabrysb@uni.coventry.ac.uk>
2016-11-30 16:54:37 +0000

    Update and rename Question 8 to Question8.py

--- Question8.py
+++ Question8.py
@@ -1,6 +1,5 @@
-INPUT string
+"""INPUT string
 strList <- list(string)
-
 
 VOWEL (list, vwl)
 	FOR i = 0 to len(list)
@@ -8,33 +7,29 @@ VOWEL (list, vwl)
 		remove element from list
 	RETURN list
 
-
 CALL vowel(strList, a)
 CALL vowel(strList, e)
 CALL vowel(strList, i)
 CALL vowel(strList, o)
 CALL vowel(strList, u)
 
-
 OUTPUT strList
-
+ Write a recursive function (pseudocode and code) that removes all vowels from a given string s. Input: "beautiful" Output: "btfl". This code takes the vowel and has a function that can remove any letter passed to it from a list made from the string input."""
 
 l = input("put in word")
 strlist = list(l)
 
-
 def vowel(lst, vwl):
-  	print(lst, vwl)
-  	for i in lst:
-    		if i == vwl:
-      			lst.remove(i)
-  	return(lst)
-      
+ 	print(lst, vwl)
+ 	for i in lst:
+   		if i == vwl:
+     			lst.remove(i)
+ 	return(lst)
+     
 lst1 = vowel(strlist, "a")
 lst2 = vowel(lst1, "e")
 lst3 = vowel(lst2, "i")
 lst4 = vowel(lst3, "o")
 lst5 = vowel(lst4, "u")
-
 
 print(lst5)
